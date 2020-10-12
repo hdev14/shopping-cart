f6445b7be6fa662f74c12a1649d9502c2fccf22e
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-12 15:36:47 -0300

    refactor: move actions to its own  file

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
-import shop from '@/api/shop'
+import actions from './actions'
 
 Vue.use(Vuex)
 
@@ -32,41 +32,7 @@ export default new Vuex.Store({
     }
   },
 
-  actions: {
-    fetchProducts ({ commit }) {
-      return new Promise((resolve, reject) => {
-        shop.getProducts(products => {
-          commit('setProducts', products)
-          resolve()
-        })
-      })
-    },
-
-    addProductToCart ({ state, commit, getters }, product) {
-      if (!getters.productIsInStock(product)) {
-        return
-      }
-
-      const cartItem = state.cart.find(item => item.id === product.id)
-
-      if (!cartItem) {
-        commit('pushProductToCart', product.id)
-      } else {
-        commit('incrementItemQuantity', cartItem)
-      }
-
-      commit('decrementProductInventory', product)
-    },
-
-    checkout ({ state, commit }) {
-      shop.buyProducts(state.cart, () => {
-        commit('emptyCart')
-        commit('setCheckoutStatus', 'success')
-      }, () => {
-        commit('setCheckoutStatus', 'fail')
-      })
-    }
-  },
+  actions,
 
   mutations: {
     setProducts (state, products) {
