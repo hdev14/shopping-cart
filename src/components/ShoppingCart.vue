7b9b65dc7dd8401adfdc2864f898ad3dae006002
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-12 14:58:51 -0300

    feat: create checkout action

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -6,6 +6,8 @@
         {{ product.title }} - {{ product.price | currency }} - {{ product.quantity }}
       </li>
       <span>TOTAL: {{ total | currency }}</span>
+      <button @click="$store.dispatch('checkout')">Checkout!</button>
+      <span v-if="$store.state.checkoutStatus">{{ $store.state.checkoutStatus }}</span>
     </ul>
   </div>
 </template>
--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,8 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     products: [],
-    cart: []
+    cart: [],
+    checkoutStatus: null
   },
 
   getters: {
@@ -51,6 +52,15 @@ export default new Vuex.Store({
       }
 
       commit('decrementProductInventory', product)
+    },
+
+    checkout ({ state, commit }) {
+      shop.buyProducts(state.cart, () => {
+        commit('emptyCart')
+        commit('setCheckoutStatus', 'success')
+      }, () => {
+        commit('setCheckoutStatus', 'fail')
+      })
     }
   },
 
@@ -72,6 +82,14 @@ export default new Vuex.Store({
 
     decrementProductInventory (state, product) {
       product.inventory--
+    },
+
+    emptyCart (state) {
+      state.cart = []
+    },
+
+    setCheckoutStatus (state, status) {
+      state.checkoutStatus = status
     }
   }
 })
